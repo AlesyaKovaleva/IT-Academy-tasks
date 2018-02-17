d1f325e247b855a054a9d9366602868a01d323d4
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2018-02-17 16:17:16 +0300

    Update 4_radius_circle.py

--- 4_radius_circle.py
+++ 4_radius_circle.py
@@ -10,6 +10,3 @@ import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius ** 2
 print("The area of the circle is: %.2f" % area)
-
-
-# print("The area of the circle is: %.2f" % (math.pi * radius ** 2))
