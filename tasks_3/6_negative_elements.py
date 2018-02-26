ea285fc3fab2310ae6c185df3f74a505277a7043
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2018-02-26 22:13:23 +0300

    add simplified solution

--- tasks_3/6_negative_elements.py
+++ tasks_3/6_negative_elements.py
@@ -15,12 +15,33 @@ while True:
 
     list_numbers.append(int(num))
 
-for i in range(len(list_numbers)):
-    if list_numbers[i] >= 0:
-        list_numbers.append(list_numbers[i])
-        list_numbers[i] = None
-
-while None in list_numbers:
-    list_numbers.remove(None)
+list_numbers.sort(key=lambda x: x >= 0)
 
 print(list_numbers)
+
+# OR
+
+# negative = []
+# positive = []
+#
+# for element in list_numbers:
+#     if element < 0:
+#         negative.append(element)
+#     else:
+#         positive.append(element)
+#
+# result_list = negative + positive
+#
+# print(result_list)
+
+# OR
+
+# for i in range(len(list_numbers)):
+#     if list_numbers[i] >= 0:
+#         list_numbers.append(list_numbers[i])
+#         list_numbers[i] = None
+#
+# while None in list_numbers:
+#     list_numbers.remove(None)
+
+# print(list_numbers)
