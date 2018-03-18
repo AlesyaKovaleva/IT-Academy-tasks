ff2aaf734a68def89ab867d91f5a9d7f76977356
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2018-03-18 16:59:49 +0300

    Done homework 8

--- tasks_8/base_of_students_3.py
+++ tasks_8/base_of_students_3.py
@@ -20,18 +20,52 @@ def make_student(name, surname, gender, age):
     }
 
 
-def main():
+def make_list_students():
     students_list = []
-    search_criteria = input('Enter the criteria to search for the student:\n'
-                            '(if there are several criteria, enter them through &): ').split('&')
 
     with open('my_file.txt', encoding='utf-8') as f:
         for line in f:
             data = line.rstrip('\n').split(',')
             students_list.append(make_student(*data))
+    return students_list
 
-    print_search(search_student(students_list, search_criteria))
+
+def main():
+    search_criteria = input('Enter the criteria to search for the student:\n'
+                            '(if there are several criteria, enter them through &): ').split('&')
+
+    print_search(search_student(make_list_students(), search_criteria))
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+
+def test_make_student():
+    result = make_student('Иван', 'Иванов', 'мужской', 32)
+    if result == {
+        'name': 'Иван',
+        'surname': 'Иванов',
+        'gender': 'мужской',
+        'age': 32,
+    }:
+        print('Test Ok')
+    else:
+        print('Test not Ok')
+
+
+def test_make_list_students():
+    result = make_list_students()
+
+    if result == [{'name': 'Петр', 'surname': 'Петров', 'gender': 'мужской', 'age': '23'},
+                  {'name': 'Сидор', 'surname': 'Сидоров', 'gender': 'мужской', 'age': '32'},
+                  {'name': 'Анна', 'surname': 'Каренина', 'gender': 'женский', 'age': '21'},
+                  {'name': 'Оби', 'surname': 'ван Кеноби', 'gender': 'мужской', 'age': '32'},
+                  {'name': 'Лара', 'surname': 'Крофт', 'gender': 'женский', 'age': '21'}]:
+        print('Test Ok')
+    else:
+        print('Test not Ok')
+
+
+# test_make_student()
+# test_make_list_students()
--- tasks_8/output_of_content_4.py
+++ tasks_8/output_of_content_4.py
@@ -9,18 +9,37 @@ import os
 path = input('Введите путь к каталогу: ')
 
 
-def list_files(path, indent=0, file=None):
+def list_files(path, file_print=None):
     if not os.path.isdir(path):
-        print('Такого каталога не существует.', file=file)
+        print('Такого каталога не существует.', file=file_print)
         return
 
-    print(' ' * indent, path, file=file)
-    for item in os.listdir(path=path):
-        if not os.path.isdir(path + '/' + item):
-            print(' ' * (indent + 4), item, file=file)
-        else:
-            list_files(path + '/' + item, indent=indent + 4, file=file)
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            directory = os.path.join(root, directory)
+            print(' ', directory, file=file_print)
+            list_files(directory, file_print=file_print)
+
+        for file in files:
+            print('    ', file, file=file_print)
 
 
 with open('dirs.txt', 'a', encoding='utf-8') as f:
-    list_files(path, file=f)
+    list_files(path, file_print=f)
+
+
+# def list_files(path, indent=0, file=None):
+#     if not os.path.isdir(path):
+#         print('Такого каталога не существует.', file=file)
+#         return
+#
+#     print(' ' * indent, path, file=file)
+#     for item in os.listdir(path=path):
+#         if not os.path.isdir(path + '/' + item):
+#             print(' ' * (indent + 4), item, file=file)
+#         else:
+#             list_files(path + '/' + item, indent=indent + 4, file=file)
+#
+#
+# with open('dirs.txt', 'a', encoding='utf-8') as f:
+#     list_files(path, file=f)
